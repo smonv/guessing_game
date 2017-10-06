352ab3fc7b88758a1c9a2f6dadae2d403117e811
Tien Thanh <tthanh.smt@gmail.com>
2017-10-06 09:46:06 +0700

    Update validate input

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,14 @@ fn main() {
             "Failed to read line",
         );
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess.value);
 
-        match guess.cmp(&secret_number) {
+        match guess.value.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
@@ -33,5 +33,23 @@ fn main() {
                 break;
             }
         }
+    }
+}
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.");
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
     }
 }
